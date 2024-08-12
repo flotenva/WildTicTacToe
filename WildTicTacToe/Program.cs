c78d6c51e63a2952070e91402f6cb28a899d3850
flotenva <flotenva@outlook.com>
2024-08-12 20:58:46 +1000

    Moved the game logic to the Game class. The Program class is responsible to display the initial menu that allows the user to start a new game, or load a saved game.

--- WildTicTacToe/Program.cs
+++ WildTicTacToe/Program.cs
@@ -4,7 +4,6 @@
     {
         public static void Main(string[] args)
         {
-            Board board = new Board();
             Menu.DisplayWelcome();      // Welcome message
             Menu.DisplayMainOptions();  // Display main options
             do
@@ -15,7 +14,7 @@
                 switch (option)
                 {
                     case 1:
-                        PlayGame(board);
+                        Game.InitialiseGame();
                         break;
                     case 2:
                         // Load Game
@@ -28,83 +27,6 @@
                         break;
                 }
             } while (true);
-        }
-
-        static void PlayGame(Board board)
-        {
-            Player player1 = null;
-            Player player2 = null;
-            bool gameWon = false;
-            int gameTurn = 0;
-            string gameWinner;
-            int gameMode;
-
-            // Select Game Mode
-            do
-            {
-                Menu.DisplayGameMode();
-                int.TryParse(Console.ReadLine(), out gameMode);
-
-                switch (gameMode)
-                {
-                    case 1:
-                        player1 = new HumanPlayer();
-                        player2 = new HumanPlayer();
-                        break;
-                    case 2:
-                        player1 = new HumanPlayer();
-                        player2 = new ComputerPlayer();
-                        break;
-                    default:
-                        Console.WriteLine("Invalid mode selected. Please try again.\n");
-                        break;
-                }
-            } while (gameMode != 1 && gameMode != 2);
-
-            // Start the game
-            while (gameTurn < 9 && !gameWon)
-            {
-                Console.Clear();
-                board.DisplayBoard();
-                // Alternate between player 1 and player 2
-                // When gameTurn is an even number, player1 makes a move
-                if (gameTurn % 2 == 0)
-                {
-                    Console.WriteLine("Player 1's turn:");
-                    player1.MakeMove(board);
-                }
-
-                // When gameTurn is an odd number, it is player2 turn
-                else
-                {
-                    Console.WriteLine("Player 2's turn:");
-                    player2.MakeMove(board);
-                }
-                gameWon = board.CheckWin();
-                gameTurn++;
-            }
-
-            Console.Clear();
-            board.DisplayBoard();
-
-            if (gameWon)
-            {
-                gameWinner = (gameTurn % 2 != 0) ? "Player 1" : "Player 2";
-                Console.WriteLine($"\nCongratulations! {gameWinner} is the winner of the WildTicTacToe game!");
-            }
-            else
-            {
-                Console.WriteLine("Oops. It's a draw!");
-            }
-            
-            // Return to main menu in 3 seconds and show countdown 3,2,1 
-            for(int i = 5; i > 0; i--)
-            {
-                Console.WriteLine($"Returning to main menu in {i}...");
-                System.Threading.Thread.Sleep(1000);
-            }
-
-            Program.Main(null);
         }
     }
 }
